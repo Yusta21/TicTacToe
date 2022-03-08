9eefdf04f2debdad4ce525abc7fa3f0420773ad6
Noel Yusta <Noelyusta@hotmail.com>
2022-03-08 17:41:35 +0100

    first try of changing size and font

--- Controller/GameView.swift
+++ Controller/GameView.swift
@@ -6,24 +6,50 @@
 //
 
 import UIKit
+import SwiftUI
 
 class GameView: UIViewController {
 
+    var buttonArray: [UIButton] = []
+    var Player :String = "X"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        Text(Player).font(.system(size: 55))
+        
+        buttonArray.append(A1)
+        buttonArray.append(A2)
+        buttonArray.append(A3)
+        buttonArray.append(B1)
+        buttonArray.append(B2)
+        buttonArray.append(B3)
+        buttonArray.append(C1)
+        buttonArray.append(C2)
+        buttonArray.append(C3)
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var A1: UIButton!
+    @IBOutlet weak var A2: UIButton!
+    @IBOutlet weak var A3: UIButton!
+    @IBOutlet weak var B1: UIButton!
+    @IBOutlet weak var B2: UIButton!
+    @IBOutlet weak var B3: UIButton!
+    @IBOutlet weak var C1: UIButton!
+    @IBOutlet weak var C2: UIButton!
+    @IBOutlet weak var C3: UIButton!
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        if sender.title(for: .normal) == nil {
+            sender.setTitle("X", for: .normal)
+            Text(Player).font(.system(size: 55)).bold()
+           
+        }
     }
-    */
-
+    
+    
 }
